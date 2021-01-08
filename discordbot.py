8da117adba4251b12011497faa964c92e5cdf00e
hangsaucer <75404533+hangsaucer@users.noreply.github.com>
2021-01-08 20:55:27 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,22 +1,22 @@
   
-from discord.ext import commands
-import os
-import traceback
+#from discord.ext import commands#
+#import os
+#import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#bot = commands.Bot(command_prefix='/')
+#token = os.environ['DISCORD_BOT_TOKEN']
 
 
-bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+#@bot.event
+#async def on_command_error(ctx, error):
+ #   orig_error = getattr(error, "original", error)
+  #  error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+   # await ctx.send(error_msg)
 
 
-bot.run(token)
+#bot.command()
+#async def ping(ctx):
+ #   await ctx.send('pong')
+
+
+#bot.run(token)
